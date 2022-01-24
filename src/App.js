b417b99d73d58cac6f07a0af49b07c9dc34a5b91
Christian Mack <cmackm@hotmail.com>
2022-01-24 21:35:37 +0100

    WIP autocomplete API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Form } from "react-bootstrap";
 
@@ -6,11 +6,29 @@ import OrganizationAutocomplete from './components/OrganizationAutocomplete.js'
 
 import './styles/App.css'
 
+const githubURL = 'https://api.github.com'
+
 function App() {
   const [selectedOrg, setOrgSelection] = useState([]);
   const [orgText, setOrgText] = useState('');
 
   const [repoFilter, setRepoFilter] = useState('');
+
+  const fetchOrgs = async () => {
+    // get the data from the api
+    const response = await fetch(`${githubUrl}/orgs/${orgText}`);
+    // convert the data to json
+    const json = await response.json();
+
+    // set state with the result
+    setData(json);
+  }
+
+  useEffect(() => {
+    if (orgText.length > 2) {
+
+    }
+  }, [orgText]);
 
   return (
     <div className="App">
