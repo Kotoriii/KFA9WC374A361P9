257e2a7acced5ed813a8956cbf5f0d3e1daaf356
Christian Mack <cmackm@hotmail.com>
2022-01-24 19:57:12 +0100

    UI for org and repos WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import { Form } from "react-bootstrap";
+
 import OrganizationAutocomplete from './components/OrganizationAutocomplete.js'
 
+import './styles/App.css'
+
 function App() {
+  const [selectedOrg, setOrgSelection] = useState([]);
+  const [orgText, setOrgText] = useState('');
+
+  const [repoFilter, setRepoFilter] = useState('');
+
   return (
     <div className="App">
-      <OrganizationAutocomplete />
+      <Form>
+        <Form.Group>
+          <OrganizationAutocomplete setInput={setOrgText} selected={selectedOrg} setSelection={setOrgSelection} />
+        </Form.Group>
+        {selectedOrg.length > 0 &&
+          <Form.Group>
+            <Form.Label>Filter by repository</Form.Label>
+            <Form.Control onChange={(event) => setRepoFilter(event.target.value)} placeholder="Repository Name" />
+          </Form.Group>
+        }
+      </Form>
     </div>
   );
 }
--- src/components/OrganizationAutocomplete.js
+++ src/components/OrganizationAutocomplete.js
@@ -1,26 +1,19 @@
-import React, { useState } from 'react';
-import { Form } from "react-bootstrap";
 import { Typeahead } from 'react-bootstrap-typeahead';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
-const OrganizationAutocomplete = (props) => {
-  const [selected, setSelection] = useState('');
-  const [text, setText] = useState('');
-
+const OrganizationAutocomplete = ({selected, setInput, setSelection}) => {
   return (
-    <Form.Group>
-      <Typeahead
-        // onChange={(selected) => {
-        //   setSelection(selected);
-        // }}
-        onInputChange={(text) => {
-          setText(text);
-        }}
-        id="organization"
-        options={["Doge", "Coin"]}
-        // selected={selected}
-      />
-    </Form.Group>
+    <Typeahead
+      onChange={(selected) => {
+        setSelection(selected);
+      }}
+      onInputChange={(text) => {
+        setInput(text);
+      }}
+      id="organization"
+      options={["Doge", "Coin"]}
+      selected={selected}
+    />
   );
 }
 
